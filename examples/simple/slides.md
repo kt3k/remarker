7efb4baf752d8774b1e3adff180d46a34236a1b2
Yoshiya Hinosawa <stibium121@gmail.com>
2017-07-19 20:45:42 +0900

    feat: add basic feature

---
+++ examples/simple/slides.md
@@ -0,0 +1,19 @@
+class: center, middle
+# My Awesome Presentation
+
+???
+
+Notes for the _first_ slide!
+
+---
+
+# Agenda
+
+1. Introduction
+2. Deep-dive
+3. ...
+
+[NOTE]: Note that you need remark.js alongside this html file, but no internet connection.
+---
+
+# Introduction
--- index.js
+++ index.js
@@ -1,8 +1,38 @@
-const berber = require('berber')
+const { asset, name, on } = require('berber')
+const layout1 = require('layout1')
+const rename = require('gulp-rename')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+require('require-yaml')
 
-berber.name('remarker')
+const script = readFileSync(join(__dirname, 'vendor/remark.js'))
+const layoutFilename = join(__dirname, 'layout.njk')
 
-berber.on('config', config => {
+const defaultCss = `
+  body {
+    font-family: 'Avenir Next', 'Hiragino Kaku Gothic ProN', 'Meiryo', 'メイリオ', sans-serif;
+  }
+  h1, h2, h3 {
+    font-weight: normal;
+  }
+  .remark-code,
+  .remark-inline-code {
+    font-family: 'Menlo', 'Monaco', 'Courier new', monospace;
+  }
+`
+
+name('remarker')
+
+on('config', config => {
   config = config || {}
-  berber.asset(config.source || 'slides.md')
+
+  asset(config.source || 'slides.md')
+    .pipe(rename({ basename: 'index', extname: '.html' }))
+    .pipe(layout1.nunjucks(layoutFilename, {
+      data: {
+        script,
+        css: config.css || defaultCss,
+        title: config.title || ''
+      }
+    }))
 })
