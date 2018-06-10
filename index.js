91516099b9b837f63a0ccc755f1f0cbcea59d4de
Yoshiya Hinosawa <stibium121@gmail.com>
2018-06-10 16:41:34 +0900

    chore: add prettier

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 #!/usr/bin/env node
 
-const { asset, dest, name, on, port, debugPagePath, helpMessage, loggerTitle, addMiddleware } = require('berber')
+const {
+  asset,
+  dest,
+  name,
+  on,
+  port,
+  debugPagePath,
+  helpMessage,
+  loggerTitle,
+  addMiddleware
+} = require('berber')
 const layout1 = require('layout1')
 const rename = require('gulp-rename')
 const { readFileSync, existsSync, statSync } = require('fs')
@@ -64,88 +74,102 @@ Options:
 See https://npm.im/remarker for more details.
 `)
 
-on('config', config => minimisted(argv => {
-  const isBuild = argv._[0] === 'build'
-  config = Object.assign({}, defaultConfig, config, argv)
+on('config', config =>
+  minimisted(
+    argv => {
+      const isBuild = argv._[0] === 'build'
+      config = Object.assign({}, defaultConfig, config, argv)
 
-  port(config.port)
-  dest(config.dest)
+      port(config.port)
+      dest(config.dest)
 
+      const slidePipeline = asset(config.source)
+        .pipe(rename({ basename: 'index', extname: '.html' }))
+        .pipe(
+          layout1.nunjucks(layoutFilename, {
+            data: {
+              css: config.css,
+              cssFiles: config.cssFiles
+                .map(url => `<link href="${url}" rel="stylesheet" />`)
+                .join('\n'),
+              script: config.script,
+              scriptFiles: config.scriptFiles
+                .map(url => `<script src="${url}"></script>`)
+                .join('\n'),
+              title: config.title,
+              remarkConfig: config.remarkConfig,
+              livereloadPort: config.livereloadPort
+            }
+          })
+        )
 
-  const slidePipeline = asset(config.source)
-    .pipe(rename({ basename: 'index', extname: '.html' }))
-    .pipe(layout1.nunjucks(layoutFilename, {
-      data: {
-        css: config.css,
-        cssFiles: config.cssFiles.map(url => `<link href="${url}" rel="stylesheet" />`).join('\n'),
-        script: config.script,
-        scriptFiles: config.scriptFiles.map(url => `<script src="${url}"></script>`).join('\n'),
-        title: config.title,
-        remarkConfig: config.remarkConfig,
-        livereloadPort: config.livereloadPort
+      // livereload settings
+      if (config.livereload && !isBuild) {
+        const livereload = require('connect-livereload')
+        const gulplivereload = require('gulp-livereload')
+        const livereloadScript = readFileSync(
+          join(__dirname, 'vendor', 'livereload.js')
+        )
+
+        const port = config.livereloadPort
+
+        addMiddleware(() => livereload({ port, src: '/livereload.js' }))
+
+        addMiddleware(() => (req, res, next) => {
+          if (require('url').parse(req.url).pathname === '/livereload.js') {
+            res.setHeader('Content-Type', 'text/javascript')
+            res.end(livereloadScript)
+          }
+          next()
+        })
+
+        gulplivereload.listen({ port })
+        slidePipeline.pipe(gulplivereload({ port }))
       }
-    }))
 
-  // livereload settings
-  if (config.livereload && !isBuild) {
-    const livereload = require('connect-livereload')
-    const gulplivereload = require('gulp-livereload')
-    const livereloadScript = require('fs').readFileSync(join(__dirname, 'vendor', 'livereload.js'))
+      asset(config.remarkPath).pipe(rename('remark.js'))
 
-    const port = config.livereloadPort
-
-    addMiddleware(() => livereload({ port, src: '/livereload.js' }))
-
-    addMiddleware(() => (req, res, next) => {
-      if (require('url').parse(req.url).pathname === '/livereload.js') {
-        res.setHeader('Content-Type', 'text/javascript')
-        res.end(livereloadScript)
-      }
-      next()
-    })
-
-    gulplivereload.listen({ port })
-    slidePipeline.pipe(gulplivereload({ port }))
-  }
-
-  asset(config.remarkPath)
-    .pipe(rename('remark.js'))
-
-  config.cssFiles.forEach(src => {
-    if (/^http/.test(src)) {
-      return
-    }
-    asset(src).base(process.cwd())
-  })
-
-  config.scriptFiles.forEach(src => {
-    if (/^http/.test(src)) {
-      return
-    }
-    asset(src).base(process.cwd())
-  })
-
-
-  config.assets.forEach(src => {
-    if (existsSync(src)) {
-      const stat = statSync(src)
-
-      if (stat.isDirectory()) {
-        asset(join(src, '**/*.*')).base(process.cwd())
-      } else if (stat.isFile()) {
+      config.cssFiles.forEach(src => {
+        if (/^http/.test(src)) {
+          return
+        }
         asset(src).base(process.cwd())
-      } else {
-        console.log(`Warning: asset entry '${src}' has unknown type, skipping this entry`)
+      })
+
+      config.scriptFiles.forEach(src => {
+        if (/^http/.test(src)) {
+          return
+        }
+        asset(src).base(process.cwd())
+      })
+
+      config.assets.forEach(src => {
+        if (existsSync(src)) {
+          const stat = statSync(src)
+
+          if (stat.isDirectory()) {
+            asset(join(src, '**/*.*')).base(process.cwd())
+          } else if (stat.isFile()) {
+            asset(src).base(process.cwd())
+          } else {
+            console.log(
+              `Warning: asset entry '${src}' has unknown type, skipping this entry`
+            )
+          }
+        } else if (src === defaultAssetsPath) {
+          // do nothing, ignore silently
+        } else {
+          console.log(
+            `Warning: asset entry '${src}' not found, skipping this entry`
+          )
+        }
+      })
+    },
+    {
+      string: ['source'],
+      alias: {
+        s: 'source'
       }
-    } else if (src === defaultAssetsPath) {
-      // do nothing, ignore silently
-    } else {
-      console.log(`Warning: asset entry '${src}' not found, skipping this entry`)
     }
-  })
-}, {
-  string: ['source'],
-  alias: {
-    s: 'source'
-  }
-}))
+  )
+)
--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const { spawn, execSync } = require('child_process')
 const { readFileSync, existsSync } = require('fs')
 const { join } = require('path')
 const rimraf = require('rimraf')
-//const spawn = require('@expo/spawn-async')
+// const spawn = require('@expo/spawn-async')
 const touch = require('touch')
 const WebSocket = require('ws')
 const axios = require('axios')
@@ -15,7 +15,7 @@ const examples = {
   hasAssets: join(__dirname, 'examples/has-assets'),
   favicon: join(__dirname, 'examples/favicon'),
   remark: join(__dirname, 'examples/remark'),
-  addScripts: join(__dirname, 'examples/add-scripts'),
+  addScripts: join(__dirname, 'examples/add-scripts')
 }
 
 const processes = []
@@ -33,38 +33,60 @@ describe('remarker', () => {
   it('creates index.html from slides.md', () => {
     execSync('node ../../index.js build', { cwd: examples.simple })
 
-    expect(readFileSync(join(examples.simple, 'build', 'index.html')).toString()).to.include('My Awesome Presentation')
+    expect(
+      readFileSync(join(examples.simple, 'build', 'index.html')).toString()
+    ).to.include('My Awesome Presentation')
   })
 
-  it('replaces stylesheets by remarker.yml\'s css property', () => {
+  it("replaces stylesheets by remarker.yml's css property", () => {
     execSync('node ../../index.js build', { cwd: examples.remark })
 
-    expect(readFileSync(join(examples.remark, 'build', 'index.html')).toString()).to.include('@import url(https://fonts.googleapis.com/css?family=Droid+Serif);')
+    expect(
+      readFileSync(join(examples.remark, 'build', 'index.html')).toString()
+    ).to.include(
+      '@import url(https://fonts.googleapis.com/css?family=Droid+Serif);'
+    )
   })
 
-  it('replaces remark.js by remarker.yml\'s remarkPath', () => {
+  it("replaces remark.js by remarker.yml's remarkPath", () => {
     execSync('node ../../index.js build', { cwd: examples.replaceRemark })
 
-    expect(readFileSync(join(examples.replaceRemark, 'build', 'remark.js')).toString()).to.include('console.log("remark.js")')
+    expect(
+      readFileSync(
+        join(examples.replaceRemark, 'build', 'remark.js')
+      ).toString()
+    ).to.include('console.log("remark.js")')
   })
 
-  it('injects scripts by remarker.yml\'s script', () => {
+  it("injects scripts by remarker.yml's script", () => {
     execSync('node ../../index.js build', {
       cwd: examples.replaceRemark
     })
 
-    expect(readFileSync(join(examples.replaceRemark, 'build', 'index.html')).toString()).to.include('console.log("injected script")')
+    expect(
+      readFileSync(
+        join(examples.replaceRemark, 'build', 'index.html')
+      ).toString()
+    ).to.include('console.log("injected script")')
   })
 
   describe('-s, --source option', () => {
-    it('specifies the slide\'s markdown path', () => {
-      execSync('node ../../index.js build --source my-slides.md', { cwd: examples.simple })
+    it("specifies the slide's markdown path", () => {
+      execSync('node ../../index.js build --source my-slides.md', {
+        cwd: examples.simple
+      })
 
-      expect(readFileSync(join(examples.simple, 'build', 'index.html')).toString()).to.include('This is my-slides.md')
+      expect(
+        readFileSync(join(examples.simple, 'build', 'index.html')).toString()
+      ).to.include('This is my-slides.md')
 
-      execSync('node ../../index.js build -s my-slides-2.md', { cwd: examples.simple })
+      execSync('node ../../index.js build -s my-slides-2.md', {
+        cwd: examples.simple
+      })
 
-      expect(readFileSync(join(examples.simple, 'build', 'index.html')).toString()).to.include('This is my-slides-2.md')
+      expect(
+        readFileSync(join(examples.simple, 'build', 'index.html')).toString()
+      ).to.include('This is my-slides-2.md')
     })
   })
 
@@ -72,7 +94,9 @@ describe('remarker', () => {
     it('are copied to build directory', () => {
       execSync('node ../../index.js build', { cwd: examples.hasAssets })
 
-      expect(existsSync('examples/has-assets/build/assets/degu.png')).to.equal(true)
+      expect(existsSync('examples/has-assets/build/assets/degu.png')).to.equal(
+        true
+      )
     })
   })
 
@@ -88,7 +112,9 @@ describe('remarker', () => {
     it('adds script files', () => {
       execSync('node ../../index.js build', { cwd: examples.addScripts })
 
-      const html = readFileSync(join(examples.addScripts, 'build', 'index.html')).toString()
+      const html = readFileSync(
+        join(examples.addScripts, 'build', 'index.html')
+      ).toString()
 
       expect(html).to.include('baz.js')
       expect(html).to.include('qux.js')
@@ -99,7 +125,9 @@ describe('remarker', () => {
     it('adds css files', () => {
       execSync('node ../../index.js build', { cwd: examples.addScripts })
 
-      const html = readFileSync(join(examples.addScripts, 'build', 'index.html')).toString()
+      const html = readFileSync(
+        join(examples.addScripts, 'build', 'index.html')
+      ).toString()
 
       expect(html).to.include('foo.css')
       expect(html).to.include('bar.css')
@@ -137,7 +165,6 @@ describe('remarker', () => {
       setTimeout(() => {
         axios.get('http://localhost:6275/index.html')
         axios.get('http://localhost:6275/livereload.js').then(res => {
-
           expect(res.data).to.include('livereload')
 
           done()
